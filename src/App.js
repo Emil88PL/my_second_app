6568e1dcb31b2fec2f6665b98699598f86bde45d
Emil <emil88pl@gmail.com>
2023-05-16 12:21:25 +0100

    Components

--- src/App.js
+++ src/App.js
@@ -7,43 +7,43 @@ import Third from './Components/Third';
 
 
 
-// function App() {
-//   return (
-//     <React.Fragment>
-//       <First />
-//       <Second />
-//       <Third />
-//     </React.Fragment>
-//   )
-// };
-
-
-// export default App;
-
-
-
-
-
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <React.Fragment>
+      <First />
+      <Second />
+      <Third />
+    </React.Fragment>
+  )
+};
+
 
 export default App;
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
