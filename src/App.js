3117e9ec1f7003090fde28c9562366da32d9375b
Emil <emil88pl@gmail.com>
2023-05-16 14:43:14 +0100

    Multiple JSX elements from Data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import First from './Components/First';
 import Second from './Components/Second';
 import Third from './Components/Third';
+import ComponentsWithArrayOfData from './Components/ComponentWithArrayOfData';
 
 
 
@@ -13,37 +13,10 @@ function App() {
       <First />
       <Second />
       <Third />
+      <ComponentsWithArrayOfData />
     </React.Fragment>
   )
 };
 
 
 export default App;
-
-
-
-
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
---
+++ src/Components/ComponentWithArrayOfData.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ComponentsWithArrayOfData = () => {
+
+    const arrayOfListItems = [
+        'Wash the dishes',
+        'Walk the dog',
+        'Clean the widnow',
+        'Put shirt on'
+    ];
+
+    const listItems = arrayOfListItems.map(  // use unique ID generator - (like in NPM packages) UUID and Shortid 
+        (item, index) => <li>{item}</li>
+    );
+
+    return (
+        <ol>
+            {listItems}
+        </ol>
+    );
+
+};
+
+export default ComponentsWithArrayOfData
