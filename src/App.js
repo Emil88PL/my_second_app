91a00b38f4c76437aaeeb94a89f3069b46379e7c
Emil <emil88pl@gmail.com>
2023-05-16 12:19:33 +0100

    Back to React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import First from './Components/First';
+import Second from './Components/Second';
+import Third from './Components/Third';
+
+
+
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <First />
+//       <Second />
+//       <Third />
+//     </React.Fragment>
+//   )
+// };
+
+
+// export default App;
+
+
+
+
+
 
 function App() {
   return (
---
+++ src/Components/Second.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Second = () => {
+    return(
+    <div>   
+        <p>My component's markup is returned explicity by the function</p>
+    </div>
+    );
+}
+
+export default Second;
+
+
