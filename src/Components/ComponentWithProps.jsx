96d10b1526801e032cee8b399a8c5b74ea4546b3
Emil <emil88pl@gmail.com>
2023-05-18 10:36:16 +0100

    Props in function body

--- src/Components/ComponentWithProps.jsx
+++ src/Components/ComponentWithProps.jsx
@@ -1,17 +1,38 @@
 
 
-function ComponentWithProps(props) {
+// function ComponentWithProps(props) {
+//     return (
+//         <>       
+//         <h1>{props.headerText}</h1>
+//         <p>{props.numericProp}</p>
+//         <p>{props.expressionProp}</p>
+//         <p>{props.valueProp}</p>
+//         <p>Showing nothing now: {props.someDataObject}</p>
+//         <p>{props.arrayProp}</p>
+//         <p>{props.functionProp}</p>
+//         </>
+//     );
+// };
+
+const ComponentWithProps = props => {
+
+    const { headerText, numericProp, expressionProp,valueProp, someDataObject, arrayProp, functionProp} = props;
+
     return (
-        <>       
-        <h1>{props.headerText}</h1>
-        <p>{props.numericProp}</p>
-        <p>{props.expressionProp}</p>
-        <p>{props.valueProp}</p>
-        <p>Showing nothing now: {props.someDataObject}</p>
-        <p>{props.arrayProp}</p>
-        <p>{props.functionProp}</p>
-        </>
-    );
-};
+            <>       
+            <h1>{headerText}</h1>
+            <p>{numericProp}</p>
+            <p>{expressionProp}</p>
+            <p>{valueProp}</p>
+            <p>Showing nothing now?!: {someDataObject} end</p>
+            <p>{arrayProp}</p>
+            <p>{functionProp}</p>
+            </>
+            );
+}
+
+
+
+
 
 export default ComponentWithProps
