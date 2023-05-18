fdd491312374aa67e1f0e6e9d904c8394ac2aec8
Emil <emil88pl@gmail.com>
2023-05-18 10:21:44 +0100

    Props 2

--- src/Components/ComponentWithProps.jsx
+++ src/Components/ComponentWithProps.jsx
@@ -7,7 +7,7 @@ function ComponentWithProps(props) {
         <p>{props.numericProp}</p>
         <p>{props.expressionProp}</p>
         <p>{props.valueProp}</p>
-        <p>Object not working ;/ will come back to that 'props.someDataObject'</p>
+        <p>Showing nothing now: {props.someDataObject}</p>
         <p>{props.arrayProp}</p>
         <p>{props.functionProp}</p>
         </>
